08f55fdd92f5e4c49f5526d51e4306972c950162
akmatoff <azim.akmatov1@gmail.com>
2021-07-30 00:02:10 +0600

    some updates

--- sketch.js
+++ sketch.js
@@ -2,46 +2,52 @@ var audio;
 var fft;
 var spectrum;
 
-var isPlaying = false;
+var isPlaying = true;
+
 var cursorX = 0;
 var cursorY = 0;
 var cursorRadius = 60;
+var cursorStrokeWeight = 1;
+var canAnimateCursorStroke = false;
 
 var bubbles = [];
 
 function preload() {
-  audio = loadSound("./assets/Sun Rain - Oils.mp3");
+  audio = loadSound("./assets/Cooroot - Wherever You Go.mp3");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   fft = new p5.FFT();
+  audio.play();
 }
 
 function drawCursor() {
   cursorX = lerp(cursorX, mouseX, 0.15);
   cursorY = lerp(cursorY, mouseY, 0.15);
 
-  fill("#f7f7f7");
+  fill("f8f8f8");
+  strokeWeight(cursorStrokeWeight);
   circle(cursorX, cursorY, cursorRadius);
 }
 
 function drawSpectrum() {
   spectrum = fft.analyze();
 
-  cursorRadius = 60 + spectrum[100] * 0.3;
+  cursorRadius = 60 + spectrum[100] * 0.5;
 
   if (spectrum[10] > 100) {
     let red = spectrum[20] * random(4);
     let green = spectrum[50] * random(10);
     let blue = spectrum[5] * random(4);
+    let alpha = random(5, 250);
 
     var bubble = {
       x: random(windowWidth),
       y: random(windowHeight),
       r: random(60, 120),
       rotateRadius: random(4),
-      color: { red, green, blue },
+      color: { red, green, blue, alpha },
       speed: random(0.01),
     };
 
@@ -49,11 +55,11 @@ function drawSpectrum() {
   }
 }
 
-function drawRect() {
+function drawBGFill() {
   fill(10, 10, 10);
 
-  if (spectrum[40] > 90) {
-    rect(0, 0, windowWidth, windowHeight);
+  if (spectrum[0] > 100) {
+    ellipse(width / 2, height / 2, 3000, 3000);
   }
 }
 
@@ -62,9 +68,15 @@ function draw() {
   cursor("none");
 
   drawSpectrum();
-  drawRect();
+  drawBGFill();
+
   bubbles.forEach((bubble) => {
-    fill(bubble.color.red, bubble.color.green, bubble.color.blue);
+    fill(
+      bubble.color.red,
+      bubble.color.green,
+      bubble.color.blue,
+      bubble.color.alpha
+    );
     circle(bubble.x, bubble.y, lerp(0, bubble.r + spectrum[50], 0.4));
 
     bubble.x +=
